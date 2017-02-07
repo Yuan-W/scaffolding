a5c611ef5f1f765300805f427f93375390a5c1e8
ZhaofengJin <zhaofengjinjeff@gmail.com>
2017-02-07 20:38:24 +0000

    add the stats_analyser

--- stats_analyser/stats_analysis.py
+++ stats_analyser/stats_analysis.py
@@ -101,5 +101,5 @@ if __name__ == '__main__':
     manager.setup(app)
     manager.add_viewdef(docs_by_exercise)
     manager.sync(app)
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5005)
 
