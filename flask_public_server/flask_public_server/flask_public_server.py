19ce13386696f1c1188f665c1d2578ec9e544118
saffikhan <saffi.khan.16@ucl.ac.uk>
2017-02-21 22:10:03 +0000

    Added access_token checks and refactored
    Added access_token checks and refactored all code around it

--- flask_public_server/flask_public_server/flask_public_server.py
+++ flask_public_server/flask_public_server/flask_public_server.py
@@ -1,135 +1,138 @@
-import httplib
-import logging.handlers
-import os,copy, sys
-import socket
-import json
-
-from flask import Flask, request, make_response, jsonify
-
-import requests, requests.exceptions
-
-import xconfig
+from flask import Flask, Response, request
+from requests import post, get
+from json import dumps
+from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
+app.debug = True
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'oauth'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-DIR = os.path.dirname(os.path.abspath(__file__))
+portA = 5000 #remote stats / hints server port
+portB = 5001 #remote stats / hints server port
+serverA = 'http://10.1.1.6:{:d}/stats/'.format(portA)
+serverB = 'http://10.1.1.6:{:d}/hints'.format(portB)
 
-# log setup
-app.logger.setLevel(logging.DEBUG)
+def fetch_user_id(token):
+    """for a given access_token returns a string user_id
+    from oauth_access_tokens table in a database, unless token has expired"""
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    sql = "SELECT `user_id`, `expires` FROM `oauth_access_tokens` WHERE `access_token`=%s"
+    cursor.execute(sql, (token, )) #SQL query for the user_id and expiration time
+    try:
+        user_id, expires = cursor.fetchone()
+    except TypeError:
+        user_id, expires = None, None
+    cursor.close()
+    connection.close()
+    if app.debug:        
+        print(user_id, expires)
+    if user_id and expires > datetime.now(): #if it was found check has it expired
+        return user_id
+    return None
 
-log_dir = os.path.join(DIR, 'logs')
-if not os.path.exists(log_dir):
-    os.mkdir(log_dir)
-handler = logging.handlers.RotatingFileHandler(
-    os.path.join(log_dir, 'site.log'),
-    maxBytes=1024 ** 2,
-    backupCount=3)
-app.logger.addHandler(handler)
+def fetch_instructor_id(username):
+    """for a given username returns an integer id from users table in a database"""
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    sql = "SELECT `id` FROM `users` WHERE `username`=%s"
+    cursor.execute(sql, (username, )) #SQL query for the instructor_id
+    instructor_id = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    if app.debug:        
+        print(instructor_id)
+    return instructor_id
 
-def get_instrcutor_id(token):
-    if token == '':
-        return 0
-    return 1
-
-# aux
-POST = 'POST'
-
-def jerror(msg=u"Invalid data!", code=400):
-    """Generic error"""
-    response = make_response(jsonify({"status": False, "message": msg}), code)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+def fetch_student_ids(username):
+    """for a given username returns a tuple with integers student_id,
+    instructor_id from users table in a database"""
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    sql = "SELECT `id`, `instructor_id` FROM `users` WHERE `username`=%s"
+    cursor.execute(sql, (username, )) #SQL query for the student_id
+    try:
+        student_id, instructor_id = cursor.fetchone()
+    except TypeError:
+        student_id, instructor_id = None, None
+    cursor.close()
+    connection.close()
+    if app.debug:        
+        print(student_id, instructor_id)
+    return student_id, instructor_id
 
 @app.route('/stats', methods=['GET'])
-def site_get():
-    token = 'dhjkadlalddajdajksdhkad'
-    instructor_id = get_instrcutor_id(token)
-    if instructor_id:
-        res = requests.get('%s/%d' % (xconfig.EXTERNAL_SERVER_URL_STATS_ANALYSIS, instructor_id))
+def forward_stats():
+    """route /stats response, looks for access_token in a header, queries
+    the db for it, and if finds an user_id, checks token expiration.
+    then finds instructor_id using user_id in db, forwards it
+    through a GET to serverA's /stats/instructor_id, accepts its reply
+    and serves it back as initial request's response"""
+    global serverA
+    if 'access_token' not in request.headers:
+        return Response("No access_token in GET header\n", status='401')
+    access_token = request.headers['access_token'].encode('utf-8')
+    if app.debug:
+        print(access_token)
+    user_id = fetch_user_id(access_token) #query db
+    if not user_id:
+        return Response("Wrong or expired access_token in GET header\n", status='401')
+    instructor_id = fetch_instructor_id(user_id) #query db
+    if not instructor_id:
+        return Response("Inconsistent DB state, access_token doesn't provide a valid user\n", status='401')
+    req = get(serverA + str(instructor_id)) #forward GET
+    if app.debug:
+        print(req.text)
+    if 'Content-Type' in req.headers and \
+      req.headers['Content-Type'] == 'application/json': #check reply
+        resp = Response(dumps(req.json()), mimetype='application/json')
+        resp.set_data(req.json()) #fill a response
+        return resp #serve it back
+    else:
+        return Response("No JSON received in stats response\n", status='404')
 
-        if res.status_code != 200:
-            return jsonify(res.text), res.status_code
-
-        # Reads data from external server
-        jdata = res.text
-
-        # Prepare the return response.
-        response = make_response(jdata, 200)
-
-        # Uses the server header
-        ctype = res.headers.get('content-type')
-
-        if ctype is not None:
-            response.headers['content-type'] = ctype
-            app.logger.info('server content-type: {0:s}'.format(ctype))
-
-        return jsonify(res.text)
-
-    return 401
-
-
-@app.route('/site', methods=['POST'])
-def site_view():
-    """Data posted to this view is in json format."""
-    app.logger.info("received [{0.path}] {0.data}".format(request))
-    
-    new_request = json.loads(request.data)
-
-    # hard coded values for now to match hints_provider input
-    new_request['student_id'] = 1
-    new_request['exercise_id'] = 101
-
-    print json.dumps(new_request)
-
-    # I could do more checks here
-    if request.json:
-        if hasattr(sys, '_called_from_test'):
-            if 'code' not in request.data or 'time_spent' not in request.data:
-                return jerror()
-
-            return jsonify(json.dumps(new_request)), 200
-
-        else:
-            try:
-                res = requests.post(xconfig.EXTERNAL_SERVER_URL,
-                                    data=json.dumps(new_request),
-                                    headers={
-                                        'Content-Type': 'application/json'
-                                    })
-            except requests.exceptions.RequestException as err:
-                msg = str(err)
-                try:
-                    msg = msg.decode('utf8')
-                except UnicodeDecodeError:
-                    msg = msg.decode('Windows-1252')
-                return jerror(msg=msg, code=500)
-
-            # It could do some processing on the data, but it just returns it to the client
-
-            # Did something go wrong on the server?
-            if res.status_code != 200:
-                return jerror()
-
-            # Reads data from external server
-            jdata = res.text
-
-            # Prepare the return response.
-            response = make_response(jdata, 200)
-
-            # Uses the server header
-            ctype = res.headers.get('content-type')
-
-            if ctype is not None:
-                response.headers['content-type'] = ctype
-                app.logger.info('server content-type: {0:s}'.format(ctype))
-
-            return response
-
-        return jerror()
-
+@app.route('/hints', methods=['POST'])
+def forward_hints():
+    """route /hints response, looks for access_token in a header, and a json
+    POST query. queries the db for access_token, and if finds an user_id,
+    checks token expiration. then queries student_id/instructor_id,
+    and if found it then forwards the POST to serverB's /hints, accepts its reply
+    and serves it back as initial request's response"""
+    global serverB
+    if 'access_token' not in request.headers:
+        return Response("No access_token in POST header\n", status='401')
+    if not 'Content-Type' in request.headers:
+        return Response("No Content-Type set in POST header\n", status='400')
+    if request.headers['Content-Type'] != 'application/json':
+        return Response("Content-Type in POST header should be application/json\n",\
+                        status='400')
+    access_token = request.headers['access_token'].encode('utf-8')
+    if app.debug:
+        print(access_token)
+    user_id = fetch_user_id(access_token) #query db
+    if not user_id:
+        return Response("Wrong or expired access_token in GET header\n", status='401')
+    student_id, instructor_id = fetch_student_ids(user_id) #query db
+    if not student_id or not instructor_id:
+        return Response("Inconsistent DB state, access_token doesn't provide a valid user\n", status='401')
+    post_data = request.json
+    post_data['student_id'] = student_id
+    post_data['instructor_id'] = instructor_id
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    req = post(serverB, json=post_data, headers=headers) #forward POST
+    if 'Content-Type' in req.headers and \
+        req.headers['Content-Type'] == 'application/json': #check reply
+        resp = Response(dumps(req.json()), mimetype='application/json')
+        #resp.set_data(req.json()) #fill a response
+        return resp #serve it back
+    else:
+        return Response("No JSON received in hints response payload\n", status='404')
 
 if __name__ == '__main__':
-    testing = False
-    app.run(host="0.0.0.0",
-            port=xconfig.SITE_SERVER_PORT,
-            debug=True)
+    app.run(host='0.0.0.0')
