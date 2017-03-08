6456ed4b3a29795c3ab5d39a46d175b8c0bcea2c
Saffi <saffi@hotmail.co.uk>
2017-03-08 09:30:58 +0000

    commiting changes added for test management

--- flask_public_server/flask_public_server/config.py
+++ flask_public_server/flask_public_server/config.py
@@ -4,8 +4,9 @@ class Development(object):
   MYSQL_DATABASE_PASSWORD = 'password'
   MYSQL_DATABASE_DB = 'oauth'
   MYSQL_DATABASE_HOST = 'localhost'
-  ADDRESS_STATS = 'http://localhost:5005/stats/'
+  ADDRESS_STATS = 'http://localhost:5005/docs/'
   ADDRESS_HINTS= 'http://localhost:5001/hints'
+  ADDRESS_TEST_MANAGEMENT= 'http://localhost:5003/testmanagement/'
 
 class Testing(Development):
   TESTING = True
@@ -16,3 +17,4 @@ class Testing(Development):
 
 class Production(object):
   DEBUG = False
+
