96c551938b0844870255b2528c83541dfffa0a6b
Yuan <yw.johnny@gmail.com>
2017-03-13 15:18:04 +0000

    Port typo fixes

--- stats_analyser/stats_analyser/config.py
+++ stats_analyser/stats_analyser/config.py
@@ -8,7 +8,7 @@ class Testing(Development):
   TESTING = True
 
 class Production(object):
-  STATS_DB_ADDRESS= 'http://admin:ANfCd8PDW8QhNAWd@10.0.1.6:5894/'
+  STATS_DB_ADDRESS= 'http://admin:ANfCd8PDW8QhNAWd@10.0.1.6:5984/'
   DEBUG = False
-  COUCHDB_SERVER = 'http://admin:ANfCd8PDW8QhNAWd@10.0.1.6:5894/'
+  COUCHDB_SERVER = 'http://admin:ANfCd8PDW8QhNAWd@10.0.1.6:5984/'
   COUCHDB_DATABASE = 'progress'
