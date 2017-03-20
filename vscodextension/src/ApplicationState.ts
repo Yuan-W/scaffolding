a1ca5441c5f626e36dcf1c12549f099895064f81
HugoDF <hdifrancesco@gmail.com>
2017-03-20 18:04:57 +0000

    Remove fixture token.

--- vscodextension/src/ApplicationState.ts
+++ vscodextension/src/ApplicationState.ts
@@ -17,9 +17,6 @@ class ApplicationState {
     }
     setTokens(tokens) {
         this.tokens = tokens;
-        // this.tokens = {
-        //     access_token: '7f05ad622a3d32a5a81aee5d73a5826adb8cbf64'
-        // };
     }
     setAuthentication(isAuthenticated) {
         this.isAuthenticated = isAuthenticated;
